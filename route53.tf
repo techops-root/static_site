2b067a5f741c1662d835067c3962b1716d1626c2
techops-root <66493344+techops-root@users.noreply.github.com>
2022-09-17 02:11:14 +0300

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,14 +1,14 @@
-data "aws_iam_policy_document" "website_policy" {
-  statement {
-    actions = [
-      "s3:GetObject"
-    ]
-    principals {
-      identifiers = ["*"]
-      type = "AWS"
-    }
-    resources = [
-      "arn:aws:s3:::${var.domain_name}/*"
-    ]
+resource "aws_route53_zone" "primary" {
+  name = var.domain_name
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name = var.domain_name
+  type = "A"
+  alias {
+    name = aws_s3_bucket.website_bucket.website_domain
+    zone_id = aws_s3_bucket.website_bucket.hosted_zone_id
+    evaluate_target_health = false
   }
 }
